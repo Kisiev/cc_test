be41b1d5ff6747b93853204cb6d4ee6e3ec6c69a
Kisiev <kisivaleri@gmail.com>
2020-11-08 12:52:23 +0300

    create tests and create job for send message to email

---
+++ tests/Feature/SendMailTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Queue;
+use App\Jobs\SendToEmailJob;
+class SendMailTest extends TestCase
+{
+	public function testValidationMessage()
+	{
+		// пустое сообещние
+		$response = $this->json('POST', '/api/mailer/sendToEmail');
+		$response->assertStatus(422);
+		$response->assertJson([
+			'errors' => [
+				'message' => ['Обязательное поле']
+			]
+		]);
+
+		// валидация длины
+		$response = $this->json('POST', '/api/mailer/sendToEmail', [
+			'message' => '12'
+		]);
+		$response->assertStatus(422);
+		$response->assertJson([
+			'errors' => [
+				'message' => ['Минимум 3 символа']
+			]
+		]);
+
+		$response = $this->json('POST', '/api/mailer/sendToEmail', [
+			'message' => Str::random(201)
+		]);
+		$response->assertStatus(422);
+		$response->assertJson([
+			'errors' => [
+				'message' => ['Максимум 200 символа']
+			]
+		]);
+	}
+	public function testSendValidMessageToEmail()
+	{
+		Queue::fake();
+
+		$response = $this->json('POST', '/api/mailer/sendToEmail', [
+			'message' => Str::random(20)
+		]);
+
+		$response->assertStatus(200);
+		Queue::assertPushed(SendToEmailJob::class);
+	}
+}
